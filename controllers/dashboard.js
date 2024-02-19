55e05a5b0297a1cde9ed17374a5ee7e06ee6d489
Kelechi Okorie <kelechiokorie22@gmail.com>
2024-02-19 17:52:51 +0100

    Added drop down menu for the user icon at the top right

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -2,8 +2,13 @@ const { User } = require('../models');
 
 const index = async (req, res) => {
     // return { name: 'kelechi', id: id };
+    let currentUser = req.user;
+
+    if(!currentUser) {
+        res.redirect('/auth/sign-in');
+    }
+    currentUser = await User.findByPk(currentUser.id);
     const users = await User.findAll();
-    const currentUser = await User.findByPk(3);
 
     console.log('the user ins hte request--', req.user, '================')
 
